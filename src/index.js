e7380cb350b49a19b82b12e43d7716c4fb59f311
sptres <shawnpark2397@gmail.com>
2024-05-05 11:29:31 -0700

    added authentication

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+// dev-p0f7h8iolhyfe0sj.us.auth0.com
+// aHldTT0KmtN558cRXRHkh2MxJMWQObjT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-p0f7h8iolhyfe0sj.us.auth0.com"
+      clientId="aHldTT0KmtN558cRXRHkh2MxJMWQObjT"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
